ee0d79401d9b822b91deed8a6c1a56004e1a642b
Xolo <solomonmanyaga@gmail.com>
2020-07-09 18:03:31 -0400

    binaru search

--- JS/sugar/naiveStringSerach.js
+++ JS/sugar/naiveStringSerach.js
@@ -1,13 +1,13 @@
 function naiveStringSearch(long, short) {
     let count = 0;
-    
+
     for (let i = 0; i < long.length; i++) {
-        const char1 = long[i];
         for (let j = 0; j < short.length; j++) {
+            const char1 = long[i + j];
             const char2 = short[j];
             if (char1 != char2) {
                 break;
-            }else if (j === short.length - 1){
+            } else if (j === short.length - 1) {
                 count += 1;
             }
         }
